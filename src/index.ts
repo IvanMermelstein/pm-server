b2486ba4f6c08064259025cd88db94741e05aafe
Iván Mermelstein <ivan.mermelstein@gmail.com>
2025-01-22 07:56:07 -0300

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/* CONFIG */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
+app.use(morgan("common"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+/* APP ROUTES */
+app.get("/", (req, res) => {
+  res.send("This is the home route");
+});
+
+/* SERVER */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
