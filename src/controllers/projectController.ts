93ca94e7a01c23c1212d18427823685670e035e4
Iván Mermelstein <ivan.mermelstein@gmail.com>
2025-01-23 07:48:29 -0300

    Add createProject

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -7,7 +7,24 @@ export const getProjects = async (req: Request, res: Response): Promise<void> =>
   try {
     const projects = await prisma.project.findMany();
     res.status(200).json(projects);
-  } catch (error) {
-    res.status(500).json({ message: "Error retrieving projects" });
+  } catch (error: any) {
+    res.status(500).json({ message: `Error retrieving projects: ${error}` });
   }
 };
+
+export const createProject = async (req: Request, res: Response): Promise<void> => {
+  const { name, description, startDate, endDate } = req.body;
+  try {
+    const project = await prisma.project.create({
+      data: {
+        name, 
+        description, 
+        startDate, 
+        endDate
+      },
+    });
+    res.status(201).json(project);
+  } catch (error: any) {
+    res.status(500).json({ message: `Error creating project: ${error}` });
+  }
+}
